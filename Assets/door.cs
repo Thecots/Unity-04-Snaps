20b909e2ea1b9464b220db4640bd9d816a06e874
DavidBorregoVW <thecots22@gmail.com>
2022-06-10 15:01:14 +0200

    first commit

---
+++ Assets/door.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    public bool open = false;
+
+
+    void Update()
+    {
+        
+    }
+}
